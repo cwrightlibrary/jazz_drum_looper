da7baae80d0511795718a80218de0d6d692eaa1c
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2023-11-02 14:00:01 -0400

    Add files via upload

--- drummer/drum_machine/drum_looper.py
+++ drummer/drum_machine/drum_looper.py
@@ -8,11 +8,6 @@ from os.path import dirname, abspath
 from sys import argv
 from drum_styles import *
 
-# --- TESTING AREA --- #
-
-
-
-# --- TESTING AREA --- #
 
 class DrumMachine:
 	def __init__(self):
@@ -25,9 +20,9 @@ class DrumMachine:
 		self.SESSION = Session(tempo=self.TEMPO)
 
 		if name == "posix":
-			self.BRUSH_SOUNDFONT = dirname(abspath(argv[0])) + "/BrushOrpheus.sf2"
+			self.BRUSH_SOUNDFONT = dirname(abspath(argv[0])) + "/asssets/BrushOrpheus.sf2"
 		elif name == "nt":
-			self.BRUSH_SOUNDFONT = dirname(abspath(argv[0])) + "\\BrushOrpheus.sf2"
+			self.BRUSH_SOUNDFONT = dirname(abspath(argv[0])) + "\\assets\\BrushOrpheus.sf2"
 		
 		self.DRUMMER = self.SESSION.new_part("Brush", soundfont=self.BRUSH_SOUNDFONT)
 
@@ -54,14 +49,6 @@ class DrumMachine:
 		self.TOGGLE_ON = "#3b8ed0"
 		self.TOGGLE_OFF = "#939ba2"
 	
-	def auto_theme(self):
-		if isDark():
-			self.TOGGLE_ON = "#1f6aa5"
-			self.TOGGLE_OFF = "#4a4d50"
-		else:
-			self.TOGGLE_ON = "#3b8ed0"
-			self.TOGGLE_OFF = "#939ba2"
-	
 	def keyboard_input(self, name, number):
 		if name == "right":
 			if self.CURRENT_STYLE < len(self.STYLES) - 1:
@@ -79,6 +66,23 @@ class DrumMachine:
 			if self.TEMPO != self.NEW_TEMPO:
 				self.TEMPO = self.NEW_TEMPO
 	
+	def update_style(self):
+		if self.CURRENT_STYLE < len(self.STYLES) - 1:
+			self.CURRENT_STYLE += 1
+		else:
+			self.CURRENT_STYLE = 0
+	
+	def update_alternate(self):
+		if self.CURRENT_ALTERNATE < 6:
+			self.CURRENT_ALTERNATE += 1
+		else:
+			self.CURRENT_ALTERNATE = 0
+	
+	def update_tempo(self, value):
+		if self.TEMPO != self.NEW_TEMPO:
+			self.TEMPO = self.NEW_TEMPO
+			self.SESSION.set_tempo_target(self.TEMPO, 4 - self.CURRENT_BEAT)
+	
 	def start_loop(self):
 		fork(play_bg, args=(self.DRUMMER,))
 		sleep(1)
@@ -95,17 +99,83 @@ class DrumMachine:
 		self.SESSION.register_keyboard_listener(on_press=self.keyboard_input, on_release=self.keyboard_output)
 
 
-class DrumGUI(CTk, DrumMachine):
-	def __init__(self):
+class DrumGUI(CTk):
+	def __init__(self, drum_machine):
 		super().__init__()
+		# fonts
+		self.font_bold_large = CTkFont(family="Noto Sans Black", size=50)
+		self.font_bold_small = CTkFont(family="Noto Sans Black", size=25)
+
+		self.font_regular_large = CTkFont(family="Noto Sans Regular", size=50)
+		self.font_regular_small = CTkFont(family="Noto Sans Regular", size=25)
+
+		self.font_emoji_large = CTkFont(family="Noto Color Emoji", size=50)
+		self.font_emoji_small = CTkFont(family="Noto Color Emoji", size=25)
+
+		# main window layout
 		self.title("jazz drummer")
 		self.geometry("500x500")
-		self.protocol("WM_DELETE_WINDOW", self.kill_loop)
+		self.grid_columnconfigure(0, weight=1)
+		self.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
+
+		# frames
+		self.frame_title = CTkFrame(self)
+		self.frame_title.grid(row=0, column=0, padx=20, pady=(0, 5), sticky="new")
+		self.frame_title.grid_columnconfigure(0, weight=1)
+
+		self.frame_tempo = CTkFrame(self)
+		self.frame_tempo.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
+		self.frame_tempo.grid_columnconfigure(0, weight=1)
+
+		# self.frame_style = CTkFrame(self)
+		# self.frame_style.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
+		# self.frame_style.grid_columnconfigure((1, 0), weight=1)
+		# self.frame_style.grid_columnconfigure((2, 0), weight=0)
+		# self.frame_style.grid_columnconfigure((2, 1), weight=1)
+		# self.frame_style.grid_columnconfigure((2, 2), weight=0)
+
+		# self.frame_instrument = CTkFrame(self)
+		# self.frame_instrument.grid(row=3, column=0, padx=20, pady=10, sticky="nsew")
+		# self.frame_instrument.grid_columnconfigure(0, weight=1)
+		# self.frame_instrument.grid_columnconfigure(1, weight=1)
+
+		# title
+		self.title_label = CTkLabel(self.frame_title, text="jazz drummer", font=self.font_bold_large)
+		self.title_label.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
+
+		# tempo
+		self.CURRENT_TEMPO = drum_machine.TEMPO
+		self.tempo_label = CTkLabel(self.frame_tempo, text=str(self.CURRENT_TEMPO) + " bpm", font=self.font_bold_small)
+		self.tempo_label.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+		self.tempo_slider = CTkSlider(self.frame_tempo, from_=60, to=200, command=self.on_click)
+		self.tempo_slider.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+		self.tempo_slider.set(self.CURRENT_TEMPO)
+		self.tempo_slider.bind("<ButtonRelease-1>", lambda update_tempo: self.update_tempo(0, drum_machine))
+
+		# style
 
 
-# --- TESTING AREA --- #
+		# instruments
 
-def run_gui():
-	pass
+	def on_click(self, value):
+		self.CURRENT_TEMPO = int(self.tempo_slider.get())
+		self.tempo_label.configure(text=str(self.CURRENT_TEMPO) + " bpm")
+	
+	def update_tempo(self, value, drum_machine):
+		if drum_machine.TEMPO != self.CURRENT_TEMPO:
+			drum_machine.TEMPO = self.CURRENT_TEMPO
+			drum_machine.SESSION.set_tempo_target(drum_machine.TEMPO, 4 - drum_machine.CURRENT_BEAT)
 
-# --- TESTING AREA --- #
+def start_drum_machine():
+	looper = DrumMachine()
+
+	def run_gui():
+		app = DrumGUI(looper)
+		app.protocol("WM_DELETE_WINDOW", looper.kill_loop)
+		app.mainloop()
+
+	fork_unsynchronized(run_gui)
+	looper.start_loop()
+
+
+start_drum_machine()
