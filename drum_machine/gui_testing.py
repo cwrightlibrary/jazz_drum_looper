70a8fc52e407db36598655a8bd175f8fb63626c8
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2023-11-10 17:58:40 -0500

    Add files via upload

---
+++ drum_machine/gui_testing.py
@@ -0,0 +1,56 @@
+from customtkinter import *
+from PIL import Image
+from os.path import join, dirname, realpath
+
+
+class App(CTk):
+	def __init__(self):
+		super().__init__()
+		self.title("gui_testing.py")
+		self.geometry("500x500")
+
+		# self._set_appearance_mode("dark")
+
+		assets_dir = join(dirname(realpath(__file__)), "assets")
+		drumset_dark_dir = join(assets_dir, "drumset_dark")
+		drumset_light_dir = join(assets_dir, "drumset_light")
+		
+		# dark_or_light_dir = drumset_dark_dir
+		dark_or_light_dir = drumset_light_dir
+		
+		self.bassf_image = CTkImage(Image.open(join(dark_or_light_dir, "bassf.png")), size=(254, 254))
+		self.basst_image = CTkImage(Image.open(join(dark_or_light_dir, "basst.png")), size=(254, 254))
+		self.bassfh_image = CTkImage(Image.open(join(dark_or_light_dir, "bassfh.png")), size=(254, 254))
+		self.bassth_image = CTkImage(Image.open(join(dark_or_light_dir, "bassth.png")), size=(254, 254))
+			
+		self.bass_drum_frame = CTkFrame(self, corner_radius=0)
+		self.bass_drum_frame.pack()
+
+		self.bass_drum_bool = False
+
+		self.bass_drum_button = CTkButton(self.bass_drum_frame, text="", image=self.bassf_image, hover=False, fg_color=("gray92", "grey14"), corner_radius=0, command=self.bass_drum_switch)
+		self.bass_drum_button.pack()
+		self.bass_drum_button.bind("<Enter>", self.on_enter)
+		self.bass_drum_button.bind("<Leave>", self.on_leave)
+
+	def bass_drum_switch(self):
+		self.bass_drum_bool = not self.bass_drum_bool
+		if self.bass_drum_bool:
+			self.bass_drum_button.configure(image=self.bassth_image)
+		else:
+			self.bass_drum_button.configure(image=self.bassfh_image)
+	
+	def on_enter(self, n):
+		if self.bass_drum_bool:
+			self.bass_drum_button.configure(image=self.bassth_image)
+		else:
+			self.bass_drum_button.configure(image=self.bassfh_image)
+	
+	def on_leave(self, n):
+		if self.bass_drum_bool:
+			self.bass_drum_button.configure(image=self.basst_image)
+		else:
+			self.bass_drum_button.configure(image=self.bassf_image)
+
+app = App()
+app.mainloop()
