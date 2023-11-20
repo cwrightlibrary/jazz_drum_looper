ba15aa78ff4ce4eae4e1dfb48c0595970f6d61c3
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2023-11-20 17:59:55 -0500

    Add files via upload

--- drum_machine/gui_testing.py
+++ drum_machine/gui_testing.py
@@ -1,4 +1,17 @@
 from customtkinter import *
+from tkinter import PhotoImage
+from os.path import dirname, join, realpath
+from darkdetect import isDark
+
+def import_image(arg):
+	if isDark():
+		df = "drumset_dark"
+	else:
+		df = "drumset_light"
+	loc = join(dirname(realpath(__file__)), "assets", df, arg)
+	img = PhotoImage(file=loc)
+
+	return img
 
 class App(CTk):
 	def __init__(self):
@@ -38,8 +51,19 @@ class App(CTk):
 		self.tabview.set("jazz")
 
 		# jazz
-		self.jazz_bass = CTkButton(self.tabview.tab("jazz"), text="bass")
-		self.jazz_bass.grid(row=0, column=0, padx=10, pady=10)
+		self.jazz_ride_icons = [import_image("ridef.png"), import_image("ridet.png"), import_image("ridefh.png"), import_image("rideth.png")]
+		self.jazz_low_tom_icons = [import_image("low_tomf.png"), import_image("low_tomt.png"), import_image("low_tomfh.png"), import_image("low_tomth.png")]
+		self.jazz_bass_icons = [import_image("bassf.png"), import_image("basst.png"), import_image("bassfh.png"), import_image("bassth.png")]
+
+		self.jazz_ride = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_ride_icons[0], fg_color="transparent", bg_color="transparent")
+		self.jazz_ride.grid(row=0, column=0, padx=(10, 250), pady=10, sticky="s")
+
+		self.jazz_low_tom = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_low_tom_icons[0], fg_color="transparent", bg_color="transparent")
+		self.jazz_low_tom.grid(row=0, column=0, padx=0, pady=10, sticky="s")
+
+		self.jazz_bass = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_bass_icons[0], fg_color="transparent", bg_color="transparent")
+		self.jazz_bass.grid(row=0, column=1, padx=10, pady=10, sticky="s")
+
 
 
 app = App()
