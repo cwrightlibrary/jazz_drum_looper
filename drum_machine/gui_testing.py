78c44044d832cc6853d14de7f4f2fad1fd5357db
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2023-11-20 19:44:04 -0500

    Add files via upload

--- drum_machine/gui_testing.py
+++ drum_machine/gui_testing.py
@@ -1,15 +1,19 @@
 from customtkinter import *
-from tkinter import PhotoImage
+from PIL import Image
 from os.path import dirname, join, realpath
 from darkdetect import isDark
 
 def import_image(arg):
-	if isDark():
-		df = "drumset_dark"
-	else:
-		df = "drumset_light"
-	loc = join(dirname(realpath(__file__)), "assets", df, arg)
-	img = PhotoImage(file=loc)
+	dd = "drumset_dark"
+	dl = "drumset_light"
+
+	dark_loc = join(dirname(realpath(__file__)), "assets", dd)
+	light_loc = join(dirname(realpath(__file__)), "assets", dl)
+
+	img_for_size = Image.open(join(dark_loc, arg))
+	size = [img_for_size.width, img_for_size.height]
+
+	img = CTkImage(dark_image=Image.open(join(dark_loc, arg)), light_image=Image.open(join(light_loc, arg)), size=size)
 
 	return img
 
@@ -25,8 +29,9 @@ class App(CTk):
 
 		# fonts
 		self.font_h1 = CTkFont(size=30, weight="bold")
-		self.font_h2 = CTkFont(size=21, weight="bold")
+		self.font_h2 = CTkFont(size=21)
 		self.font_p = CTkFont(size=16)
+		self.font_p_bold = CTkFont(size=16, weight="bold")
 
 		# frames
 		self.sidebar_frame = CTkFrame(self, corner_radius=0)
@@ -51,19 +56,26 @@ class App(CTk):
 		self.tabview.set("jazz")
 
 		# jazz
-		self.jazz_ride_icons = [import_image("ridef.png"), import_image("ridet.png"), import_image("ridefh.png"), import_image("rideth.png")]
-		self.jazz_low_tom_icons = [import_image("low_tomf.png"), import_image("low_tomt.png"), import_image("low_tomfh.png"), import_image("low_tomth.png")]
-		self.jazz_bass_icons = [import_image("bassf.png"), import_image("basst.png"), import_image("bassfh.png"), import_image("bassth.png")]
+		self.tabview.tab("jazz").grid_columnconfigure(0, weight=1)
+		self.jazz_frame_instruments = CTkFrame(self.tabview.tab("jazz"))
+		self.jazz_frame_instruments.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+		self.jazz_frame_instruments.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+		self.jazz_frame_instruments.grid_columnconfigure(0, weight=1)
 
-		self.jazz_ride = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_ride_icons[0], fg_color="transparent", bg_color="transparent")
-		self.jazz_ride.grid(row=0, column=0, padx=(10, 250), pady=10, sticky="s")
+		self.jazz_label_instruments = CTkLabel(self.jazz_frame_instruments, text="instruments", font=self.font_h2)
+		self.jazz_label_instruments.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
-		self.jazz_low_tom = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_low_tom_icons[0], fg_color="transparent", bg_color="transparent")
-		self.jazz_low_tom.grid(row=0, column=0, padx=0, pady=10, sticky="s")
+		self.jazz_bass_switch = CTkSwitch(self.jazz_frame_instruments, text="bass", font=self.font_p)
+		self.jazz_bass_switch.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
 
-		self.jazz_bass = CTkButton(self.tabview.tab("jazz"), text="", image=self.jazz_bass_icons[0], fg_color="transparent", bg_color="transparent")
-		self.jazz_bass.grid(row=0, column=1, padx=10, pady=10, sticky="s")
+		self.jazz_low_tom_switch = CTkSwitch(self.jazz_frame_instruments, text="low tom", font=self.font_p)
+		self.jazz_low_tom_switch.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
 
+		self.jazz_mid_toms_switch = CTkSwitch(self.jazz_frame_instruments, text="mid toms", font=self.font_p)
+		self.jazz_mid_toms_switch.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
+
+		self.jazz_snare_switch = CTkSwitch(self.jazz_frame_instruments, text="snare", font=self.font_p)
+		self.jazz_snare_switch.grid(row=4, column=0, padx=5, pady=5, sticky="nsew")
 
 
 app = App()
